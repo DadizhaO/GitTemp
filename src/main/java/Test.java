521fcbb0d5599d7969cbb6c3f415fe571d840a9f
Sanya <sanya.dadizha@gmail.com>
2018-12-27 19:58:22 +0200

    merge

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Commit");
         System.out.println("Br 1 commit");
-        System.out.println("Br 1 second commit");
     }
 }
