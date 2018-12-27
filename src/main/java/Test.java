9ad22f7fc0d9f5b42f44ad3a5d3042a663ed2355
Sanya <sanya.dadizha@gmail.com>
2018-12-27 20:03:05 +0200

    sq 3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("First commit, changes in br2");
+        System.out.println("sq 1");
+        System.out.println("sq 1 ocm");
+        System.out.println("sq 1 vob");
     }
 }
