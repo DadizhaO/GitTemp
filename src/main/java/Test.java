84a457589a0d919e6100e99d34d9fb866a82d230
Sanya <sanya.dadizha@gmail.com>
2018-12-27 21:35:47 +0200

    br3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Commit");
         System.out.println("Br 1 commit");
+        System.out.println("Branch 3 ");
     }
 }
